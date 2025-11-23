676e1900a4aa97fd1f45f70f9f1c2a927737e254
Troy-Simpson-TJ <troysimpson689@gmail.com>
2025-11-22 23:12:51 -0500

    Add dynamic port support and startup log to server
    Server now listens on a port defined by the PORT environment variable, defaulting to 3000. Added a startup log message to confirm the server is running on the correct port.

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ app.use(function(err, req, res, next) {
   );
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
 
 console.log('Server running at http://localhost:3000/');
 
